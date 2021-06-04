f4c500f2a775b280bc1c783600dcfd9413169fdb
zhoy <artyomzhoy@gmail.com>
2021-06-04 11:44:18 +0300

    Add search activity/fragment

--- app/src/main/java/com/android/animesearch/ui/AnimeSearchFragment.java
+++ app/src/main/java/com/android/animesearch/ui/AnimeSearchFragment.java
@@ -1,11 +1,14 @@
 package com.android.animesearch.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.SearchView;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,7 +17,7 @@ import com.android.animesearch.R;
 public class AnimeSearchFragment extends Fragment {
 
     private Button mSearchButton;
-    private SearchView mSearchView;
+    private EditText mSearchView;
 
     public static AnimeSearchFragment newInstance () {
         return new AnimeSearchFragment();
@@ -26,13 +29,29 @@ public class AnimeSearchFragment extends Fragment {
     mSearchButton = (Button) view.findViewById(R.id.button_search);
     mSearchButton.setOnClickListener(new View.OnClickListener() {
         @Override
-        public void onClick(View v) {
-
+        public void onClick(View view) {
+            Intent intent = new Intent(getActivity(), AnimeListActivity.class);
+            startActivity(intent);
         }
     });
 
-    mSearchView = (SearchView) view.findViewById(R.id.search_view);
+    mSearchView = (EditText) view.findViewById(R.id.search_view);
+    mSearchView.addTextChangedListener(new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    });
 
     return view;
     }
