8e38a6a593a4a887f518fc478595d1c930aad226
zhoy <artyomzhoy@gmail.com>
2021-06-22 00:16:08 +0300

    main branch check

--- app/src/main/java/com/android/animesearch/ui/AnimeListFragment.java
+++ app/src/main/java/com/android/animesearch/ui/AnimeListFragment.java
@@ -28,7 +28,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.UUID;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
